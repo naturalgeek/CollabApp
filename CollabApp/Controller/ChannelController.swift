b83cb589e1816bdd62a535e547972057f02398c3
Silicium <silicium@asis.local>
2018-12-01 01:43:16 +0100

    only display joined channels

--- CollabApp/Controller/ChannelController.swift
+++ CollabApp/Controller/ChannelController.swift
@@ -7,6 +7,9 @@ class ChannelController: ClientAccesingControllerBase {
         self.getClient().conversationsList() { response, error in
             for (_,subJson):(String, JSON) in response {
                 let channel:Channel = Channel()
+                if subJson["is_member"].bool == false {
+                    continue
+                }
                 // IM channels.
                 if subJson["name"].stringValue.isEmpty {
                     channel.setName(subJson["user"].stringValue)
